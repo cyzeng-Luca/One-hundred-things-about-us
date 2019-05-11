779d3a5642351ece0d84fc215d808cd9e5ff4767
cyzeng <luca_z@sina.com>
2019-05-11 18:09:25 +0800

    测试husky

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import { AppRegistry } from "react-native";
 import App from "./App.tsx";
 import { name as appName } from "./app.json";
 
+debugger;
 console.log(1);
 AppRegistry.registerComponent(appName, () => App);
