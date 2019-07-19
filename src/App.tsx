50f1cfa22c011ea74724fc3dfef59f60ca5a32c3
cyzeng <luca_z@sina.com>
2019-07-19 18:24:29 +0800

    ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BaseComponent } from "@utils/constructMethods";
-// import { BaseComponent } from "./shared/utils/constructMethods";
 import Content from "./Content";
 
 export default class App extends BaseComponent {
